83451aeb12b7e4f964fa02ff3576ef7b7e053419
Prashanth Kumar <prashusiri.1990@gmail.com>
2021-04-28 19:39:57 +1000

    Account details component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'employee-dashboard';
   showMain = true;
-  employeeDashboardTemplate = false;
   emailTemplate = false;
   bankingTemplate = false;
 
   showTemplate(name: string) {
-    this.employeeDashboardTemplate = false;
     this.emailTemplate = false;
 
-    if (name.toUpperCase() === 'EDT') {
-      this.employeeDashboardTemplate = true;
-    } else if (name.toUpperCase() === 'ET') {
+    if (name.toUpperCase() === 'ET') {
       this.emailTemplate = true;
     } else if (name.toUpperCase() === 'BT' ) {
       this.bankingTemplate = true;
