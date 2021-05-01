00218e9921d1b3aef220fb1ce23ec6927236a0de
Prashanth Kumar <prashusiri.1990@gmail.com>
2021-05-01 13:34:57 +1000

    Personal details Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  showMain = true;
+  showMain = true; //TODO should be true
   emailTemplate = false;
-  bankingTemplate = false;
+  bankingTemplate = false; //TODO should be false
 
   showTemplate(name: string) {
     this.emailTemplate = false;
 
     if (name.toUpperCase() === 'ET') {
       this.emailTemplate = true;
-    } else if (name.toUpperCase() === 'BT' ) {
+    } else if (name.toUpperCase() === 'BT') {
       this.bankingTemplate = true;
     }
 
--- src/app/modules/banking/banking.module.ts
+++ src/app/modules/banking/banking.module.ts
@@ -1,57 +1,52 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BankingDashboardComponent } from './components/banking-dashboard/banking-dashboard.component';
-import { RouterModule, Routes } from "@angular/router";
-import { ModalComponent } from "./components/common/modal/modal.component";
-import { BankingContentComponent } from "./components/banking-content/banking-content.component";
-import { BankingHomeComponent } from "./components/banking-home/banking-home.component";
-import { BankingNavComponent } from "./components/banking-nav/banking-nav.component";
-import { SvgIconComponent } from "./components/common/svg-icon/svg-icon.component";
+import { RouterModule, Routes } from '@angular/router';
+import { ModalComponent } from './components/common/modal/modal.component';
+import { BankingContentComponent } from './components/banking-content/banking-content.component';
+import { BankingHomeComponent } from './components/banking-home/banking-home.component';
+import { BankingNavComponent } from './components/banking-nav/banking-nav.component';
+import { SvgIconComponent } from './components/common/svg-icon/svg-icon.component';
 import { AccountsComponent } from './components/accounts/accounts.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
 import { AccountDetailsComponent } from './components/account-details/account-details.component';
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 import { FundTransferComponent } from './components/fund-transfer/fund-transfer.component';
 import { TransferComponent } from './components/transfer/transfer.component';
 import { PayAnyoneComponent } from './components/pay-anyone/pay-anyone.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { CreditCardsComponent } from './components/credit-cards/credit-cards.component';
 import { DebitCardsComponent } from './components/debit-cards/debit-cards.component';
-
+import { PersonalDetailsComponent } from './components/personal-details/personal-details.component';
 
 const routes: Routes = [
-    {path: 'dashboard', component: BankingDashboardComponent},
-    {path: 'accounts', component: AccountsComponent},
-    {path: 'fundTransfer', component: FundTransferComponent},
-    {path: 'cards', component: CardsComponent}
+  { path: 'dashboard', component: BankingDashboardComponent },
+  { path: 'accounts', component: AccountsComponent },
+  { path: 'fundTransfer', component: FundTransferComponent },
+  { path: 'cards', component: CardsComponent },
+  { path: 'details', component: PersonalDetailsComponent },
 ];
 
 @NgModule({
-    declarations: [
-        BankingDashboardComponent,
-        BankingContentComponent,
-        BankingHomeComponent,
-        BankingNavComponent,
-        SvgIconComponent,
-        ModalComponent,
-        AccountsComponent,
-        TransactionsComponent,
-        AccountDetailsComponent,
-        FundTransferComponent,
-        TransferComponent,
-        PayAnyoneComponent,
-        CardsComponent,
-        CreditCardsComponent,
-        DebitCardsComponent
-    ],
-    exports: [
-        BankingHomeComponent
-    ],
-    imports: [
-        CommonModule,
-        RouterModule.forRoot(routes),
-        FormsModule
-    ]
+  declarations: [
+    BankingDashboardComponent,
+    BankingContentComponent,
+    BankingHomeComponent,
+    BankingNavComponent,
+    SvgIconComponent,
+    ModalComponent,
+    AccountsComponent,
+    TransactionsComponent,
+    AccountDetailsComponent,
+    FundTransferComponent,
+    TransferComponent,
+    PayAnyoneComponent,
+    CardsComponent,
+    CreditCardsComponent,
+    DebitCardsComponent,
+    PersonalDetailsComponent,
+  ],
+  exports: [BankingHomeComponent],
+  imports: [CommonModule, RouterModule.forRoot(routes), FormsModule],
 })
-export class BankingModule {
-}
+export class BankingModule {}
