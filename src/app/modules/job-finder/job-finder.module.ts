56d63a8cd0f1460b9daa9377a2096ad1b8857c1c
Prashanth Kumar <prashusiri.1990@gmail.com>
2023-11-22 10:11:03 +0530

    #6 View option for jobs on home screen

--- src/app/modules/job-finder/job-finder.module.ts
+++ src/app/modules/job-finder/job-finder.module.ts
@@ -9,6 +9,10 @@ import { SuggestedJobComponent } from './components/suggested-job/suggested-job.
 import { PopularCompanyComponent } from './components/popular-company/popular-company.component';
 import { FeaturedJobComponent } from './components/featured-job/featured-job.component';
 import { FaqComponent } from './components/faq/faq.component';
+import { MyJobsComponent } from './components/my-jobs/my-jobs.component';
+import { PostJobComponent } from './components/post-job/post-job.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProfileComponent } from './components/profile/profile.component';
 import { JobFinderService } from './service/job-finder.service';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -19,6 +23,9 @@ const routes = [
 		children: [
 			{ path: 'home', component: LandingComponent },
 			{ path: 'faq', component: FaqComponent },
+			{ path: 'my-jobs', component: MyJobsComponent },
+			{ path: 'post-job', component: PostJobComponent },
+			{ path: 'profile', component: ProfileComponent },
 			{ path: '**', pathMatch: 'full', redirectTo: 'home' },
 		],
 	},
@@ -33,10 +40,15 @@ const routes = [
 		PopularCompanyComponent,
 		FeaturedJobComponent,
 		FaqComponent,
+		MyJobsComponent,
+		PostJobComponent,
+		ProfileComponent,
 	],
 	imports: [
 		CommonModule,
 		SharedModule,
+		FormsModule,
+		ReactiveFormsModule,
 		RouterModule.forChild(routes),
 		HttpClientModule,
 	],
